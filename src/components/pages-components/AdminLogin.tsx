21eb1ba71ca6e8860fac6a260e674d771d4e9a78
Nestor Quinones <ndqa96@gmail.com>
2024-08-14 15:22:09 -0400

    fix: fixing login

--- src/components/pages-components/AdminLogin.tsx
+++ src/components/pages-components/AdminLogin.tsx
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../styles/Login.css';
 import '../../styles/index.css'
 import darkLogo from '../../assets/images/logo-dark.png';
-import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
-import { Login } from '../../utils/types/loginTypes';
+import { FieldValues, useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 
 function AdminLogin() {
     const { 
         register, 
         handleSubmit, 
         formState: { errors },
+        reset,
         watch
     } = useForm();
 
+    const [inputPasswordType, setInputPasswordType] = useState('password');
+
+    const eyeStyle = {
+        color: '#7f7e7f'
+    };
+
+    const [eyes, setEyes] = useState(false);
+
+    const handleEyes = (): void => {
+        if (inputPasswordType === 'password') {
+            setInputPasswordType('text');
+        }
+        if (inputPasswordType === 'text') {
+            setInputPasswordType('password');
+        }
+        setEyes(!eyes);
+    };
+
     //On submit login form
-    const onSubmit = (data: FieldValues) => {
+    const onSubmit = (data: FieldValues): void => {
         console.log(data);
-        return data;
+        reset();
     }
     console.log(watch('holi', errors));
     
@@ -36,6 +55,7 @@ function AdminLogin() {
                     <label htmlFor="email">Email</label>
                     <input
                         className='input-dark'
+                        placeholder='email@tuemail.com'
                         {...register("email", { 
                             required: true,
                             pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
@@ -49,19 +69,29 @@ function AdminLogin() {
                 <div className="input-container">
                     <label htmlFor="email">Contraseña</label>
                     <input
+                        placeholder='Contraseña'
                         className='input-dark'
+                        type={inputPasswordType}
                         {...register("password", { required: true })} 
                     />
                     {errors.password && <span className='input-error'>La contraseña es requerida para iniciar sesión</span>}
+                    <div 
+                        onClick={()=> handleEyes()}
+                        className='eye-container'
+                    >
+                        {!eyes && <AiFillEye style={eyeStyle}/>}
+                        {eyes && <AiFillEyeInvisible style={eyeStyle}/>}                
+                    </div>
                 </div>
                 {/* errors will return when field validation fails  */}
 
                 <div className="input-container-terms">
-                    <label>
+                    <label className="custom-checkbox">
                         <input
                             type="checkbox"
                             {...register('remember')}
                         />
+                        <span className="checkmark"></span>
                         Recuérdame
                     </label>
                     <Link to={"/forget"} className='sample-link'>¿Olvidaste tu contraseña?</Link>

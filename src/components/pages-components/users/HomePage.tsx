cc4e75ea61fd65f652cf9df4035d99f3748dbdc5
Nestor Quinones <ndqa96@gmail.com>
2024-09-16 16:10:21 -0400

    fix: fixing marquee

--- src/components/pages-components/users/HomePage.tsx
+++ src/components/pages-components/users/HomePage.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../../../styles/homepage.css';
 import { HomePageHeader } from './HomePageHeader';
+import { Marquee } from './Marquee';
 
 export const HomePage = () => {
   return (
     <div className='homepage-header-container'>
         <HomePageHeader />
+        <Marquee />
     </div>
   )
 }
---
+++ src/components/pages-components/users/Marquee.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { BsEmojiWink } from "react-icons/bs";
+import '../../../styles/marquee.css';
+
+export const Marquee = () => {
+    return (
+        <div className="marquee">
+            <div className="marquee-text">
+                <BsEmojiWink style={{
+                    marginRight: '1em',
+                    marginLeft: '1em'
+                }} /> <span>Obtén un mes de prueba gratis</span>
+            </div>
+            <div className="marquee-text">
+                <BsEmojiWink style={{
+                    marginRight: '1em',
+                    marginLeft: '1em'
+                }} /> <span>Obtén un mes de prueba gratis</span>
+            </div>
+            <div className="marquee-text">
+                <BsEmojiWink style={{
+                    marginRight: '1em',
+                    marginLeft: '1em'
+                }} /> <span>Obtén un mes de prueba gratis</span>
+            </div>
+            <div className="marquee-text">
+                <BsEmojiWink style={{
+                    marginRight: '1em',
+                    marginLeft: '1em'
+                }} /> <span>Obtén un mes de prueba gratis</span>
+            </div>
+            <div className="marquee-text">
+                <BsEmojiWink style={{
+                    marginRight: '1em',
+                    marginLeft: '1em'
+                }} /> <span>Obtén un mes de prueba gratis</span>
+            </div>
+            <div className="marquee-text">
+                <BsEmojiWink style={{
+                    marginRight: '1em',
+                    marginLeft: '1em'
+                }} /> <span>Obtén un mes de prueba gratis</span>
+            </div>
+            <div className="marquee-text">
+                <BsEmojiWink style={{
+                    marginRight: '1em',
+                    marginLeft: '1em'
+                }} /> <span>Obtén un mes de prueba gratis</span>
+            </div>
+            <div className="marquee-text">
+                <BsEmojiWink style={{
+                    marginRight: '1em',
+                    marginLeft: '1em'
+                }} /> <span>Obtén un mes de prueba gratis</span>
+            </div>
+        </div>
+    )
+}

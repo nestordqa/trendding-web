8fa11fa9446b0aed995bff8775c7e82b62757c18
Nestor Quinones <ndqa96@gmail.com>
2024-09-16 16:27:06 -0400

    fix: adding trust companies

--- src/components/pages-components/users/HomePage.tsx
+++ src/components/pages-components/users/HomePage.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import '../../../styles/homepage.css';
 import { HomePageHeader } from './HomePageHeader';
 import { Marquee } from './Marquee';
+import { TrustCompanies } from './TrustCompanies';
 
 export const HomePage = () => {
   return (
     <div className='homepage-header-container'>
         <HomePageHeader />
         <Marquee />
+        <TrustCompanies />
     </div>
   )
 }
---
+++ src/components/pages-components/users/TrustCompanies.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import TraddingView from '../../../assets/images/TradingView.png';
+import MetaTrader from '../../../assets/images/MetaTrader.png';
+import Discord from '../../../assets/images/Discord.png';
+import Forex from '../../../assets/images/Forex.png';
+import MyFxBook from '../../../assets/images/MyFxbook.png';
+import '../../../styles/trustsCompanies.css';
+
+export const TrustCompanies = () => {
+  return (
+    <div className='trust-container'>
+        <div className="chip-container">
+            <span>COMPAÑÍAS QUE CONFÍAN EN NOSOTROS</span>
+        </div>
+        <div className="companies-container">
+            <img src={TraddingView} alt="Tradding View" className="" />
+            <img src={MetaTrader} alt="MetaTrader" className="" />
+            <img src={Discord} alt="Discord" className="" />
+            <img src={Forex} alt="Forex Factory" className="" />
+            <img src={MyFxBook} alt="MyFxbook" className="" />
+        </div>
+    </div>
+  )
+}
